db27297efad8cc670eab7d5bd8bce20a0550fe73
joahangf11 <joahangarciaf@hotmail.com>
2024-04-22 19:27:56 -0600

    Se agrega la base del componente Service

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,8 +1,24 @@
 // Container with the information of the service, problem and buttons
+import "../styles/service.css";
+import Button from '@mui/material/Button';
 
 const Service = (props) => {
     return (
-        <div></div>
+        <section className="service">
+            <div className="titulo">
+                <h2>Servicio</h2>
+            </div>
+            <div className="tarjetaProblema">
+                <h2>Problema</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel leo ac nulla accumsan sollicitudin. Donec pellentesque nisl et nunc malesuada sollicitudin. Integer pharetra iaculis elit, vel dictum tortor vehicula ac. Phasellus sagittis erat sed dolor pellentesque pellentesque</p>
+                <p>SOI</p>
+            </div>
+            <div className="botones">
+                <Button variant="outlined">Ayuda soporte</Button>
+                <Button variant="outlined">Reporte</Button>
+                <Button variant="outlined" color="error">Ayuda supervisor</Button>
+            </div>
+        </section>
     );
 }
 
--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,25 +1,28 @@
 // Componente que funje de contenedor del resto de la pantalla
 import "../styles/window.css";
 import Service from "./Service";
+import Stats from "./Stats";
+import Solutions from "./Solutions"
+import Client from "./Client"
 
 const Window = (props) => {
     return (
         <div className="window">
             <div className="stats">
                 <div className="title"></div>
-                
+                <Stats/>
             </div>
             <div className="client">
                 <div className="title"></div>
-                
+                <Client/>
             </div>
             <div className="service">
                 <div className="title"></div>
-                {Service}
+                <Service/>
             </div>
             <div className="solutions">
                 <div className="title"></div>
-
+                <Solutions/>
             </div>
         </div>
     );
