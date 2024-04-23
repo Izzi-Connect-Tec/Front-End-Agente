0eed039b6a1fa2e4aaa8de038224130141d7d231
karixcm <a01661547@tec.mx>
2024-04-23 14:20:43 -0600

    se agrega cliente

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,10 +1,25 @@
 // Container with the description and information of the client
+import "../styles/client.css"
 
 const Client = (props) => {
     return (
         <div>
             <div className="titulo">
                 <h2>Cliente</h2>
+            </div>
+            <div className="client-div">
+                <p>Nombre: Joahan Lecona</p>
+                <p>Localidad:
+                Atizapan de Zaragoza</p>
+                <p>Plan contratado:
+                Izzi Basic
+                $150</p>
+                <p>Historial de atención:</p>
+                <ul>
+                    <li>Problemas con internet.</li>
+                    <li>Sin señal</li>
+                    <li>Problemas con internet.</li>
+                </ul>
             </div>
         </div>
     );
