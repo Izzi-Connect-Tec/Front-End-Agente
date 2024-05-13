07fa7c1776fb75a52af3452edd84ce2c4dbaf281
karixcm <a01661547@tec.mx>
2024-05-13 16:31:25 -0600

    LogIn-v2
    Mejora en el aspecto, perfeccionamiento en botones, gif con pausa y agregación de logo, así como borrado de archivos innecesarios.
    Queda pendiente:
    - Alerta de credenciales no encontradas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/login.css';
-import giphy from '../elements/abstract.gif';
+import React, { useState, useEffect } from "react";
+import "../styles/login.css";
+import giphy from "../elements/pinkdots.gif";
+import logo from "../elements/izziN.png";
 
 const Login = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -22,15 +23,47 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <img src={giphy} alt="Animación de carga" className={`background-gif ${gifLoaded ? 'visible' : ''}`} />
-      <div className={`background-overlay ${showLogin && gifLoaded ? 'blurred' : ''}`}></div>
-      <div className={`login-form ${showLogin && gifLoaded ? 'visible' : ''}`}>
-        <h2>Iniciar Sesión</h2>
-        <form>
-          <input type="text" placeholder="Usuario" disabled={!showLogin || !gifLoaded} />
-          <input type="password" placeholder="Contraseña" disabled={!showLogin || !gifLoaded} />
-          <button type="submit" disabled={!showLogin || !gifLoaded}>Iniciar Sesión</button>
-        </form>
+      <img
+        src={giphy}
+        alt="Gif de fondo"
+        className={`background-gif ${gifLoaded ? "visible" : ""}`}
+      />
+      <div
+        className={`background-overlay ${
+          showLogin && gifLoaded ? "blurred" : ""
+        }`}
+      ></div>
+      <div className={`login-form ${showLogin && gifLoaded ? "visible" : ""}`}>
+        <h2 className="title">Bienvenido</h2>
+        <div>
+          <img className="logo" src={logo} alt="logoIzziConnect"></img>
+        </div>
+        <p className="txt">Ingrese sus credenciales para accesar.</p>
+        <div className="formdiv">
+          <form className="form" align="center">
+            <input
+              align="center"
+              className="input"
+              type="text"
+              placeholder="Usuario"
+              disabled={!showLogin || !gifLoaded}
+            />
+            <input
+              align="center"
+              className="input"
+              type="password"
+              placeholder="Contraseña"
+              disabled={!showLogin || !gifLoaded}
+            /><button
+            className="button"
+            type="submit"
+            disabled={!showLogin || !gifLoaded}
+          >
+            Iniciar Sesión
+          </button>
+          </form>
+        </div>
+        
       </div>
     </div>
   );
