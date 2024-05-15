b770652df94ccebe15181d830afe561575d0057d
Lorenitaaaa <loreindm@hotmail.com>
2024-05-15 12:29:01 -0600

    cards, profilepic y prueba de gráficas
    gio reporte (lloré un poquito)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Window from './components/Window';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Calificacion from './components/Calificacion';
+
+
+
 
 function App() {
   return (
     <div className="App">
-        <Window/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Window/>}/>
+          <Route path="/calificacion" element={<Calificacion/>}/>
+         
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
