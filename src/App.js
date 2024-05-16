ec46547d51f339770ee58b9589a9abf179af9906
Lorenitaaaa <loreindm@hotmail.com>
2024-05-15 23:42:39 -0600

    acomodé las cochinadas esas ;( (otra vez)
    cards, header, pp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa BrowserRouter
 import Window from './components/Window';
-<<<<<<< HEAD
 import Login from './components/Login';
 import Calificacion from './components/Calificacion';
 
@@ -20,27 +19,3 @@ function App() {
 }
 
 export default App;
-
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Calificacion from './components/Calificacion';
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Window/>}/>
-          <Route path="/calificacion" element={<Calificacion/>}/>
-         
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
--- src/components/Calificacion.jsx
+++ src/components/Calificacion.jsx
@@ -1,68 +1,10 @@
-// import '../styles/calificacion.css';
-// import Card from 'react-bootstrap/Card';
-// import StarRating from './StarRating';
-// import ProfilePhoto from './ProfilePicture'; 
-// import izziImage from '../styles/izzi.jpeg'; 
-
-// const Calificacion = () => {
-//   // Dummy data
-//   const data = [
-//     { title: 'Calificación promedio', value: 5, rank: '#1 Joahan' },
-//     { title: 'Cantidad de llamadas en el día', value: '10', rank: '#1 Pepo' },
-//     { title: 'Promedio de tiempo en llamada', value: '7', rank: '#1 Alfy' },
-//     { title: 'Cantidad de llamadas en el día', value: 4, rank: '#1 Benny' } // Nueva tarjeta
-//   ];
-
-//   // Colores de izzi
-//   const colors = ['#00BCB4', '#D7006D', '#FFCE00', '#EC6907'];
-
-//   // Ruta de la imagen dummy
-//   const profilePhotoUrl = izziImage;
-
-//   return (
-//     <div className="calificacion-container">
-//       {/* Paso la ruta de la imagen de muestra como prop */}
-//       <ProfilePhoto photoUrl={profilePhotoUrl} />
-//       {data.map(({ title, value, rank }, index) => (
-//         <Card
-//           key={index}
-//           style={{ backgroundColor: colors[index] }}
-//           className={`card mb-2 ${index === 3 ? 'orange-card' : ''}`}
-//         >
-//           <Card.Body>
-//             <div className="card-content">
-//               <div className="card-title">{title}</div>
-//               {index === 0 ? ( 
-//                 <div className="card-value">
-//                   <StarRating rating={value} />
-//                 </div>
-//               ) : (
-//                 <div className="card-value">{value}</div>
-//               )}
-//               <div className="card-rank">{rank}</div>
-//             </div>
-//           </Card.Body>
-//         </Card>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Calificacion;
-
-//intento gráfica (no definitivo priettes)
 import '../styles/calificacion.css';
 import Card from 'react-bootstrap/Card';
 import StarRating from './StarRating';
 import ProfilePhoto from './ProfilePicture'; 
-<<<<<<< HEAD
 import izziImage from '../elements/izzi.jpeg';
 import AgentRankingChart from './AgentRankingChart';
 import Header from './Header';
-=======
-import izziImage from '../styles/izzi.jpeg';
-import AgentRankingChart from './AgentRankingChart'; // Importamos el componente de la gráfica
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
 
 const Calificacion = () => {
   // Dummy data
@@ -70,85 +12,65 @@ const Calificacion = () => {
     { title: 'Calificación promedio', value: 5, rank: '#1 Joahan' },
     { title: 'Cantidad de llamadas en el día', value: '10', rank: '#1 Pepo' },
     { title: 'Promedio de tiempo en llamada', value: '7', rank: '#1 Alfy' },
-<<<<<<< HEAD
     { title: 'Cantidad de llamadas en el día', value: 4, rank: '#1 Benny' } 
-=======
-    { title: 'Cantidad de llamadas en el día', value: 4, rank: '#1 Benny' } // Nueva tarjeta
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
   ];
 
   // Colores de izzi
   const colors = ['#00BCB4', '#D7006D', '#FFCE00', '#EC6907'];
 
-<<<<<<< HEAD
-  // Ruta de la imagen dummy (ya en elements)
-=======
-  // Ruta de la imagen dummy
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
   const profilePhotoUrl = izziImage;
 
   return (
-    <div className="calificacion-container">
-<<<<<<< HEAD
-      <Header/>
-      <ProfilePhoto photoUrl={profilePhotoUrl} />
-      <div className="card-container">
-        {data.slice(0, 3).map(({ title, value, rank }, index) => (
-          <Card
-            key={index}
-            style={{ backgroundColor: colors[index] }}
-            className={`card mb-2 ${index === 3 ? 'orange-card' : ''}`}
-          >
-            <Card.Body>
-              <div className="card-content">
-                <div className="card-title">{title}</div>
-                {index === 0 ? ( 
-                  <div className="card-value">
-                    <StarRating rating={value} />
-                  </div>
-                ) : (
-                  <div className="card-value">{value}</div>
-                )}
-                <div className="card-rank">{rank}</div>
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-=======
-      <ProfilePhoto photoUrl={profilePhotoUrl} />
-      {data.map(({ title, value, rank }, index) => (
-        <Card
-          key={index}
-          style={{ backgroundColor: colors[index] }}
-          className={`card mb-2 ${index === 3 ? 'orange-card' : ''}`}
-        >
-          <Card.Body>
-            <div className="card-content">
-              <div className="card-title">{title}</div>
-              {index === 0 ? ( 
-                <div className="card-value">
-                  <StarRating rating={value} />
+    <div className="calificacion-page">
+      <Header />
+      <div className="calificacion-content">
+        <div className="profile-container">
+          <ProfilePhoto photoUrl={profilePhotoUrl} />
+        </div>
+        <div className="card-container">
+          {data.slice(0, 3).map(({ title, value, rank }, index) => (
+            <Card
+              key={index}
+              style={{ backgroundColor: colors[index] }}
+              className="card mb-2"
+            >
+              <Card.Body>
+                <div className="card-content">
+                  <div className="card-title">{title}</div>
+                  {index === 0 ? ( 
+                    <div className="card-value">
+                      <StarRating rating={value} />
+                    </div>
+                  ) : (
+                    <div className="card-value">{value}</div>
+                  )}
+                  <div className="card-rank">{rank}</div>
                 </div>
-              ) : (
-                <div className="card-value">{value}</div>
-              )}
-              <div className="card-rank">{rank}</div>
-            </div>
-          </Card.Body>
-        </Card>
-      ))}
-      {/* Integramos el componente de la gráfica aquí */}
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
       <div className="chart-container">
         <AgentRankingChart agentData={data.slice(0, 3)} />
       </div>
+      {/* Aquí va la tarjetita naranja huérfana */}
+      <div className="orange-card-container">
+        <Card
+          style={{ backgroundColor: colors[3] }}
+          className="card mb-2 orange-card"
+        >
+          <Card.Body>
+            <div className="card-content">
+              <div className="card-title">{data[3].title}</div>
+              <div className="card-value">{data[3].value}</div>
+              <div className="card-rank">{data[3].rank}</div>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
-<<<<<<< HEAD
-  
-=======
->>>>>>> b770652df94ccebe15181d830afe561575d0057d
 };
 
 export default Calificacion;
