a10cec044f2578533d4a01bbd8f8429873d2e539
karixcm <a01661547@tec.mx>
2024-05-22 16:14:03 -0600

    Actualización de vista de notificaciones, función para borrar y función para mostrar la cantidad correcta de notificaciones

---
+++ src/components/Notificacion.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/notification.css';
+import { TiDelete } from "react-icons/ti";
+
+const Notificacion = (props) => {
+    return (
+        <div className="not-container">
+            <div>
+                <TiDelete className='not-del' onClick={props.onDelete} />
+            </div>
+            <div className='not-description'>
+                <p>{props.descripcion}</p>
+            </div>
+            <div className='not-sender'>
+                <p>From: {props.remitente}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Notificacion;
+
