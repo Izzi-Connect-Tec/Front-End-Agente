e329a711dfa583babd106d748eba0bdcef68703d
abner-lecona <abner_lecona@outlook.com>
2024-05-28 12:34:18 -0600

    Commit antes de intentar jalar datos

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -28,4 +28,29 @@ const Solutions = () => {
   );
 };
 
+// const Solutions = () => {
+//   return (
+//     <div className="solutions">
+//       <div className="titulo-soluciones">
+//         <h2>Soluciones</h2>
+//       </div>
+//       <div className="solutions-div">
+//         <div className="possible-solutions">
+//           {solutionsData.map((solution) => (
+//             <SolutionCard
+//               key={solution.IdSolucion}
+//               solutionId={solution.IdSolucion}
+//               tituloSolucion={solution.Nombre}
+//             />
+//           ))}
+//         </div>
+//         <div className="botones-solutions">
+//           <FormDialog />
+//           <FormDialog />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 export default Solutions;
