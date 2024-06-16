778097704b7d14fd6bdca4fc4b4b58334054d3ce
joahangf11 <joahangarciaf@hotmail.com>
2024-06-16 15:37:18 -0600

    Version preliminar 0.2

--- src/components/AmazonConnect.jsx
+++ src/components/AmazonConnect.jsx
@@ -389,7 +389,7 @@ const formatDuration = (duration) => {
   return (
   // <div id="ccp" style={{ width: "400px", height: "250px" }}>
   <section>
-    <div id="ccp" style={{ display: "block"}}/>
+    <div id="ccp" style={{ display: "none"}}/>
     {/* <button onClick={muteAgent}>MUTE</button>
     <button onClick={unmuteAgent}>UNMUTE</button>
     <button onClick={acceptCall}>ACEPTAR LLAMADA</button>
