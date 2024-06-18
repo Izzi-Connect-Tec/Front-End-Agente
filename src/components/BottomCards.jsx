39422ebbd51e5169222715a47ec3ccea91f24bf6
joahangf11 <joahangarciaf@hotmail.com>
2024-06-18 07:18:39 -0600

    correcion de control llamada

--- src/components/BottomCards.jsx
+++ src/components/BottomCards.jsx
@@ -66,11 +66,7 @@ const BottomCards = () => {
     <div className="customContainer">
       <div className="customRow">
         <div className="customCol2">
-          <div>
-            <div>
               <CallControlsCard />
-            </div>
-          </div>
         </div>
         <div className="customCol">
           <div className="customCard cardPrimary">
