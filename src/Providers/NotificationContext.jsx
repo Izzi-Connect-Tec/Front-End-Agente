d9fbb631b649373a322ce890439fd3fed85bf377
karixcm <a01661547@tec.mx>
2024-06-18 01:43:45 -0600

    Borrado de documentos por errores encontrados

--- src/Providers/NotificationContext.jsx
+++
@@ -1,8 +0,0 @@
-// Author: Karla Cruz
-// Context provider for notifications.
-
-import { createContext } from "react";
-
-const NotificationContext = createContext();
-
-export default NotificationContext;
